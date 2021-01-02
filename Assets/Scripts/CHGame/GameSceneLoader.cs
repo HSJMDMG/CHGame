6ffd2140c9bbbda8288514e8b5ec18af241a23a3
Ying Lu <HSJMDMG@users.noreply.github.com>
2021-01-02 02:22:43 +0100

    add project files

---
+++ Assets/Scripts/CHGame/GameSceneLoader.cs
@@ -0,0 +1,22 @@
+﻿namespace CHGame
+{
+    using UnityEngine;
+    using UnityEngine.SceneManagement;
+
+    /// <summary>
+    /// Simple class encapsulating load scene method.
+    /// </summary>
+    public class GameSceneLoader : MonoBehaviour
+	{
+	    public void LoadScene(string a_nextScene)
+	    {
+	        SceneManager.LoadScene(a_nextScene);
+	    }
+	}
+}
+
+
+
+
+
+
