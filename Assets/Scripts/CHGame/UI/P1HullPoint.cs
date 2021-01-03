6295319e89dde088bc7d869f7b4453a86bba4bb3
Ying Lu <HSJMDMG@users.noreply.github.com>
2021-01-03 16:50:21 +0100

    follow GameCreation.doc. Add point prefab and segment mesh

---
+++ Assets/Scripts/CHGame/UI/P1HullPoint.cs
@@ -0,0 +1,13 @@
+namespace CHGame
+{
+    using System;
+    using UnityEngine;
+    using UnityEngine.UI;
+
+    public class P1HullPoint : MonoBehaviour
+    {
+      void Update() {
+
+      }
+    }
+}
