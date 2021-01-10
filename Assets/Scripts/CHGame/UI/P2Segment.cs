5fdff0a18cd0b4da5d0e75add0c57bf65e6bfdd0
Ying Lu <HSJMDMG@users.noreply.github.com>
2021-01-10 16:26:12 +0100

    prepare to change branch

---
+++ Assets/Scripts/CHGame/UI/P2Segment.cs
@@ -0,0 +1,19 @@
+namespace CHGame
+{
+    using UnityEngine;
+    using Util.Geometry;
+
+    public class P1HullSegment : MonoBehaviour
+    {
+        public LineSegment Segment { get; set; }
+
+        //private P1AreaGameController m_gameController;
+
+        void Awake()
+        {
+            //m_gameController = FindObjectOfType<P1AreaGameController>();
+        }
+
+
+    }
+}
